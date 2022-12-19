895dc8553abafbb79c45fca56a4395e6ba4cb760
omkam <omardyfan@gmail.com>
2022-12-18 21:21:06 -0500

    changes made from theia lab environment

---
+++ src/components/CurrencyList.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const CurrencyList = (props) => {
+    const { dispatch,remaining  } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+
+            if(cost > remaining) {
+                alert("The value cannot exceed remaining funds  £"+remaining);
+                setCost("");
+                return;
+            }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
+
+    return (
+        <div>
+            <div className='row'>
+
+            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend">
+                <label className="input-group-text" htmlFor="inputGroupSelect01">Currency</label>
+                </div>
+                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        <option value="Dollar" name="dollar">$Dollar</option>
+                        <option value="Pound" name="pound">£Pound</option>
+                        <option value="Euro" name="euro">€Euro</option>
+                        <option value="Rupee" name="rupee">₹ Rupee</option>
+                       
+                  </select>
+
+                    
+
+                    
+                </div>
+                </div>
+
+        </div>
+    );
+};
+
+export default CurrencyList;
